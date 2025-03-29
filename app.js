2194613f8fbb8c467ff05d2887997285a666c534
Narvik1221 <107913962+Narvik1221@users.noreply.github.com>
2025-03-29 10:15:55 +0600

    fix ./app

--- app.js
+++ app.js
@@ -1,26 +1,27 @@
+// app.js
 const express = require("express");
 const bodyParser = require("body-parser");
-const cors = require("cors"); // Добавлено для поддержки CORS
+const cors = require("cors");
 const db = require("./models");
 require("dotenv").config();
 const bcrypt = require("bcryptjs");
 const path = require("path");
+
+// Функция создания администратора, если он не существует
 async function createAdminUser() {
   const adminExists = await db.User.findOne({ where: { admin: true } });
-
   if (!adminExists) {
     const hashedPassword = await bcrypt.hash("admin", 10);
-
     await db.User.create({
       firstName: "admin",
       lastName: "admin",
       password: hashedPassword,
       admin: true,
     });
-
     console.log("Администратор создан.");
   }
 }
+
 // Импорт роутов
 const authRoutes = require("./routes/authRoutes");
 const eventRoutes = require("./routes/eventRoutes");
@@ -28,8 +29,8 @@ const favoriteRoutes = require("./routes/favoriteRoutes");
 const adminRoutes = require("./routes/adminRoutes");
 const profileRoutes = require("./routes/profileRoutes");
 const categoryRoutes = require("./routes/categoryRoutes");
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Разрешаем CORS для всех источников и методов
 app.use(
@@ -50,10 +51,9 @@ app.use("/api/favorites", favoriteRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/profile", profileRoutes);
 app.use("/api/categories", categoryRoutes);
-// Синхронизация базы данных и запуск сервера
 
+// Дополнительный маршрут для списка загруженных файлов
 const fs = require("fs");
-
 app.get("/list-uploads", (req, res) => {
   const uploadsDir = path.join(__dirname, "uploads", "events");
   fs.readdir(uploadsDir, (err, files) => {
@@ -63,14 +63,17 @@ app.get("/list-uploads", (req, res) => {
     res.json({ files });
   });
 });
+
+// Синхронизация базы данных и создание администратора
 db.sequelize
   .sync({ alter: true })
   .then(async () => {
     await createAdminUser();
-    app.listen(PORT, "0.0.0.0", () => {
-      console.log(`Сервер запущен на порту ${PORT}`);
-    });
+    console.log("База данных синхронизирована, администратор создан.");
   })
   .catch((err) => {
     console.error("Ошибка подключения к базе данных:", err);
   });
+
+// Экспортируем Express-приложение без вызова app.listen()
+module.exports = app;
